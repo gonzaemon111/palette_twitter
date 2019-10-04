c8f1d68eb711b3301c13a3934180bbf81f4eb4e8
gonzaemon111 <goki14914@icloud.com>
2019-10-05 08:05:34 +0900

    add routing for configuration at first step

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users/sign_up", to: "users#new"
+  post "users/sign_in", to: "users#sign_in"
 end
