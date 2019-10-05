8914b5905acb3e11724243d689966be4a4f5a346
gonzaemon111 <goki14914@icloud.com>
2019-10-05 22:31:47 +0900

    add some methods for application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def sign_in(token)
+    cookies.permanent[:token] = token
+    current_user
+  end
+
+  def current_user
+    User.find_by(token: cookies[:token])
+  end
 end
