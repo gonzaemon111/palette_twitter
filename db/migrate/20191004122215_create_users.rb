182ff26e6b636c4181426e95732fae1cefe0655f
gonzaemon111 <goki14914@icloud.com>
2019-10-05 22:33:12 +0900

    some change create user table

--- db/migrate/20191004122215_create_users.rb
+++ db/migrate/20191004122215_create_users.rb
@@ -1,13 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email, null: false, comment: "Eメールアドレス"
+      t.string :email, null: false, unique: true, comment: "Eメールアドレス"
       t.string :password_digest, null: false, limit: 255, comment: "パスワード"
       t.string :nickname, index: true, null: false, default: "", comment: "ニックネーム"
+      t.string :token, null: false, comment: "トークン"
 
       t.timestamps
     end
 
-    add_index :users, %i[email password_digest], name: "complex_unique_uid", unique: true
+    add_index :users, %i[email password_digest], name: "complex_unique_uid"
   end
 end
