c7db7133130a91f2ab86d811dc655c215101cc3b
gonzaemon111 <goki14914@icloud.com>
2019-10-05 22:32:55 +0900

    add configuration for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "users/sign_up", to: "users#new"
-  post "users/sign_in", to: "users#sign_in"
+  get "users/signup", to: "users#new"
+  post "users/signup", to: "users#sign_up"
+  resources :tweets
 end
