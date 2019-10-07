f1d3936e58e9edbc530c2de7076fb7600712798d
gonzaemon111 <goki14914@icloud.com>
2019-10-07 19:49:33 +0900

    add app/controllers for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,16 @@
 class TweetsController < ApplicationController
   def index
-    
+    tweets = Tweet.all.order(:updated_at)
+    @tweets = TweetDecorator.decorate_collection(tweets)
   end
 
   def new
-    @tweet = Tweet.new
+    tweet = Tweet.new
+    @tweet = TweetDecorator.decorate(tweet)
+  end
+
+  def create
+    Rails.logger.debug "params: #{params}"
+    redirect_to "/"
   end
 end
