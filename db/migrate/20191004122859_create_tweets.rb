df054cc06c2a3719b273c776ab97bcfdc091df02
gonzaemon111 <goki14914@icloud.com>
2019-10-07 17:20:15 +0900

    some change migration file for create_tweets table

--- db/migrate/20191004122859_create_tweets.rb
+++ db/migrate/20191004122859_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.references :users, index: true, null: false, comment: "ユーザーID"
+      t.references :user, foreign_key: true, index: true, null: false, comment: "ユーザーID"
       t.string :content, index: true, null: false, default: "", comment: "内容"
       t.text :image, comment: "画像"
 
