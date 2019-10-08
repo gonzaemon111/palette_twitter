ea39962374cb405fe1c99e623bb3ec061f181536
gonzaemon111 <goki14914@icloud.com>
2019-10-09 02:57:03 +0900

    add migration file for create tweets table

--- db/migrate/20191004122859_create_tweets.rb
+++ db/migrate/20191004122859_create_tweets.rb
@@ -4,6 +4,7 @@ class CreateTweets < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true, index: true, null: false, comment: "ユーザーID"
       t.string :content, index: true, null: false, default: "", comment: "内容"
       t.text :image_data, comment: "画像"
+      t.integer :tid, index: true, null: false, default: 0, comment: "ツイート ID"
 
       t.timestamps
     end
