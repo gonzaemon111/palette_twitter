862a243e2c77bfc543203418ca50c7aaa738adee
gonzaemon111 <goki14914@icloud.com>
2019-10-11 01:36:14 +0900

    add configuration routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "users/signup", to: "users#new"
-  post "users/signup", to: "users#signup"
-  get "users/signin", to: "users#signin_input"
-  post "users/signin", to: "users#signin"
-  delete "users/signout", to: "users#signout"
-  resources :users, only: %i[show edit update destroy]
-  resources :tweets
+  resources :users, only: %i[show edit update destroy] do
+    collection do
+      get :signup, to: "users#new"
+      post :signup
+      get :signin, to: "users#signin_input"
+      post :signin
+      delete :signout
+    end
+  end
+  resources :tweets do
+    collection do
+      post "create_retweet"
+    end
+  end
 end
